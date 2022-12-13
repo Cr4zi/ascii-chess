43356d76eaa6d8f9155a9cfb4726945cc3b015b1
Cr4zi <ori.mizrahi1000@gmail.com>
2022-12-13 21:52:12 +0200

    Printing pawn pos

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,6 @@ fn main() {
     let black_pawn = pieces::Pawn { pos: "e10", color: pieces::Color::Black };
     let white_pawn = pieces::Pawn { pos: "e10", color: pieces::Color::White};
 
-    println!("black pawn: {:#?}", {&black_pawn.draw().unwrap()});
-    println!("white pawn: {:#?}", {&white_pawn.draw().unwrap()});
+    println!("black pawn: img {}, pos {}", {&black_pawn.draw().unwrap()}, {&black_pawn.pos});
+println!("white pawn: img {}, pos {}", {&white_pawn.draw().unwrap()}, {&white_pawn.pos});
 }
